c80d1bb93f1b72da7386deb85dd213aeae185db7
Alexey Ksenofontov <alexksenofontov@gmail.com>
2019-10-03 00:06:38 +0300

    flag_pole2

--- flag.py
+++ flag.py
@@ -11,7 +11,7 @@ pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
-flagpole()
+flag_pole()
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
