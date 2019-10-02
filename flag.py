5e4d94cabc45864c2c2b7af39506c123b60fe2c6
Alexey Ksenofontov <alexksenofontov@gmail.com>
2019-10-02 23:58:22 +0300

    flag_pole

--- flag.py
+++ flag.py
@@ -1,11 +1,18 @@
 import pygame
 
 
+def flag_pole():
+    pygame.draw.rect(screen, (255, 255, 255), (100, 100, 10, 400), 0)
+    pygame.draw.rect(screen, (255, 255, 255), (95, 500, 22, 5), 0)
+
+
 pygame.init()
 
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
+flagpole()
+pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
 pygame.quit()
