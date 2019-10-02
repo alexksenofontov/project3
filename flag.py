073d8ee5869bb82337770134556ff2727c395306
Alexey Ksenofontov <alexksenofontov@gmail.com>
2019-10-03 00:16:23 +0300

    blue_pole

--- flag.py
+++ flag.py
@@ -6,12 +6,17 @@ def flag_pole():
     pygame.draw.rect(screen, (255, 255, 255), (95, 500, 22, 5), 0)
 
 
+def blue_pole():
+    pygame.draw.rect(screen, (0, 0, 255), (110, 200, 300, 100), 0)
+
+
 pygame.init()
 
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
 flag_pole()
+blue_pole()
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pass
